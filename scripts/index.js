ef72e161dac00aa6a7f4d722b2096e2ace102ff8
Justin Pot <justin.pot92@gmail.com>
2025-05-14 22:59:31 -0700

    add delete buttion, style, and create js to remove cards from container

--- scripts/index.js
+++ scripts/index.js
@@ -64,6 +64,11 @@ function getCardElement(data) {
     cardLikeBtnEl.classList.toggle("card__like-button_active");
   });
 
+  const cardDeleteBtnEl = cardElement.querySelector(".card__delete-button");
+  cardDeleteBtnEl.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
   return cardElement;
 }
 
